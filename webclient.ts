9e3357ace5ec30fd0431791a01ff3354dcf96bd0
DaInfLoop <github@dainfloop.is-a.dev>
2026-07-06 18:46:01 +0100

    add bot client and bot-as-user client

--- webclient.ts
+++ webclient.ts
@@ -1,8 +1,20 @@
 import "dotenv/config";
 import { WebClient } from "@slack/web-api";
+import { App } from "@slack/bolt";
 
 export const webClient = new WebClient(process.env.XOXC, {
     headers: {
         'Cookie': `d=${process.env.XOXD}`        
     }
 });
+
+export const userClient = new WebClient(process.env.XOXP);
+
+export const botClient = new App({
+    token: process.env.XOXB,
+    signingSecret: process.env.SIGNING_SECRET,
+    appToken: process.env.XAPP,
+    socketMode: true
+});
+
+botClient.start().then(() => console.log("Bolt.JS client up!"));
